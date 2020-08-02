206411807c9b8c06a28f6d00daa2111b794876ff
choedeb <choedeb@gmail.com>
2020-08-03 01:40:00 +0900

    [UPDATE] Project Activity & Fragment

---
+++ app/src/main/java/com/ddd4/dropit/ui/detail/DetailFragment.kt
@@ -0,0 +1,12 @@
+package com.ddd4.dropit.ui.detail
+
+import com.ddd4.dropit.R
+import com.ddd4.dropit.base.ui.BaseFragment
+import com.ddd4.dropit.databinding.FragmentDetailBinding
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+
+    override fun setBind() {
+
+    }
+}
---
+++ app/src/main/java/com/ddd4/dropit/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.ddd4.dropit.ui.main
+
+import androidx.hilt.lifecycle.ViewModelInject
+import com.ddd4.dropit.base.ui.BaseViewModel
+
+class MainViewModel @ViewModelInject constructor(): BaseViewModel() {
+
+}
