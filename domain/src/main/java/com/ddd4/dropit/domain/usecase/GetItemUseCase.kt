15ceb70a04259f26130a7b33f276587e172c1144
choedeb <choedeb@gmail.com>
2020-08-10 05:38:47 +0900

    [Fix] DI 모듈 수정
    모듈 컴포넌트 변경
    변경된 Data, Domain에 따라 Provide 수정

--- domain/src/main/java/com/ddd4/dropit/domain/usecase/GetItemUseCase.kt
+++ domain/src/main/java/com/ddd4/dropit/domain/usecase/GetItemUseCase.kt
@@ -1,15 +1,14 @@
 package com.ddd4.dropit.domain.usecase
 
-import com.ddd4.dropit.domain.repository.DropitRepository
-import com.ddd4.dropit.domain.Result
 import com.ddd4.dropit.domain.entity.DomainEntity
+import com.ddd4.dropit.domain.repository.DropitRepository
 import java.lang.Exception
 
-class GetItemUseCase(
+class getItemUseCase(
     private val dropitRepository: DropitRepository
 ) {
 
-    suspend fun execute(folderId: Long): Result<List<DomainEntity.Item>> =
+    suspend fun execute(folderId: Long): Result =
         try {
             dropitRepository.getItems(folderId).let { data ->
                 Result.Success(data)
@@ -17,4 +16,9 @@ class GetItemUseCase(
         } catch (e: Exception) {
             Result.Error(e)
         }
+
+    sealed class Result {
+        data class Success(val data: List<DomainEntity.Item>) : Result()
+        data class Error(val e: Throwable) : Result()
+    }
 }
