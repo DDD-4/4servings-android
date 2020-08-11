d8c6eba485048e3c37737c74ee01b89a329f04c3
choedeb <choedeb@gmail.com>
2020-08-12 05:56:24 +0900

    Fix. Scope Function 수정
    Scope Function을 리시버를 반환하도록 변경 (also -> run)

--- presentation/src/main/java/com/ddd4/dropit/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/ddd4/dropit/presentation/ui/main/MainViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.*
 import com.ddd4.dropit.domain.Result
 import com.ddd4.dropit.domain.entity.DomainEntity
 import com.ddd4.dropit.domain.usecase.GetFolderUseCase
-
 import com.ddd4.dropit.presentation.base.ui.BaseViewModel
 import com.ddd4.dropit.presentation.entity.PresentationEntity
 import com.ddd4.dropit.presentation.mapper.mapToPresentation
@@ -21,6 +20,7 @@ class MainViewModel @ViewModelInject constructor(
     val folderItems : LiveData<List<PresentationEntity.Folder>> = _folderItems
 
     val startFolder = SingleLiveEvent<Long>()
+
     val startCategory = SingleLiveEvent<Long>()
 
     init {
