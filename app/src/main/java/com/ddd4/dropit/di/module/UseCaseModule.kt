4f283aecf4023c3e3b064727a3c07a59fadaa1ee
choedeb <choedeb@gmail.com>
2020-08-12 05:46:05 +0900

    Fix. UseCase 클래스명, Result 클래스 분리
    각 UseCase 명칭이 파스칼케이스에 맞지 않아서 대문자로 수정, DI Module 변경
    UseCase 내에 공통적으로 있던 Result 클래스 분리

--- app/src/main/java/com/ddd4/dropit/di/module/UseCaseModule.kt
+++ app/src/main/java/com/ddd4/dropit/di/module/UseCaseModule.kt
@@ -1,8 +1,8 @@
 package com.ddd4.dropit.di.module
 
 import com.ddd4.dropit.domain.repository.DropitRepository
-import com.ddd4.dropit.domain.usecase.getFolderUseCase
-import com.ddd4.dropit.domain.usecase.getItemUseCase
+import com.ddd4.dropit.domain.usecase.GetFolderUseCase
+import com.ddd4.dropit.domain.usecase.GetItemUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,13 +15,13 @@ object UseCaseModule {
 
     @Provides
     @Singleton
-    fun provideFolderUseCase(dropitRepository: DropitRepository): getFolderUseCase {
-        return getFolderUseCase(dropitRepository)
+    fun provideFolderUseCase(dropitRepository: DropitRepository): GetFolderUseCase {
+        return GetFolderUseCase(dropitRepository)
     }
 
     @Provides
     @Singleton
-    fun provideItemUseCase(dropitRepository: DropitRepository): getItemUseCase {
-        return getItemUseCase(dropitRepository)
+    fun provideItemUseCase(dropitRepository: DropitRepository): GetItemUseCase {
+        return GetItemUseCase(dropitRepository)
     }
 }
--- domain/src/main/java/com/ddd4/dropit/domain/BaseUseCase.kt
+++
@@ -1,5 +0,0 @@
-package com.ddd4.dropit.domain
-
-interface BaseUseCase<Params, Type> {
-    //suspend operator fun invoke(params: Params? = null): Type
-}
