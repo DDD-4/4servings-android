e7a2169eaef49fdcfb5a65fb4fecc687c97b4c26
choedeb <choedeb@gmail.com>
2020-08-30 01:44:05 +0900

    update. main ui

--- presentation/src/main/java/com/ddd4/dropit/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/ddd4/dropit/presentation/ui/main/MainViewModel.kt
@@ -4,47 +4,87 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.ddd4.dropit.domain.Result
 import com.ddd4.dropit.domain.entity.DomainEntity
+import com.ddd4.dropit.domain.usecase.GetCategoryUseCase
 import com.ddd4.dropit.domain.usecase.GetFolderUseCase
+import com.ddd4.dropit.domain.usecase.SetSectionUseCase
 import com.ddd4.dropit.presentation.base.ui.BaseViewModel
 import com.ddd4.dropit.presentation.entity.PresentationEntity
 import com.ddd4.dropit.presentation.mapper.mapToPresentation
+import com.ddd4.dropit.presentation.util.ItemClickListener
 import com.ddd4.dropit.presentation.util.SingleLiveEvent
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class MainViewModel @ViewModelInject constructor(
+    private val setSectionUseCase: SetSectionUseCase,
+    private val getCategoryUseCase: GetCategoryUseCase,
     private val getFolderUseCase: GetFolderUseCase
 ): BaseViewModel() {
+
+    private val _categoryItems = MutableLiveData<List<PresentationEntity.Category>>()
+    val categoryItems : LiveData<List<PresentationEntity.Category>> = _categoryItems
 
     private val _folderItems = MutableLiveData<List<PresentationEntity.Folder>>()
     val folderItems : LiveData<List<PresentationEntity.Folder>> = _folderItems
 
-    val startFolder = SingleLiveEvent<Long>()
+    private val _folderState = MutableLiveData<Boolean>(false)
+    val folderState : LiveData<Boolean> = _folderState
 
-    val startCategory = SingleLiveEvent<Long>()
+    val categoryClick = SingleLiveEvent<Long>()
+
+    val folderClick = SingleLiveEvent<Long>()
+
+    val addClick = SingleLiveEvent<Void>()
 
     init {
+        setSectionFromJson()
         getFolderItems()
+    }
+
+    private fun setSectionFromJson() {
+        viewModelScope.launch {
+            when (val result = setSectionUseCase.execute()) {
+                is Result.Success -> getCategoryItems()
+                is Result.Error -> Timber.d(result.exception)
+            }
+        }
+    }
+
+    private fun getCategoryItems() {
+        viewModelScope.launch {
+            when (val result = getCategoryUseCase.execute()) {
+                is Result.Success -> _categoryItems.value = result.data.map(DomainEntity.Category::mapToPresentation)
+                is Result.Error -> Timber.d(result.exception)
+            }
+        }
     }
 
     private fun getFolderItems() {
         viewModelScope.launch {
-            getFolderUseCase.execute().run {
-                when (this) {
-                    is Result.Success -> {
-                        if (data.isNotEmpty()) {
-                            _folderItems.value = data.map(DomainEntity.Folder::mapToPresentation)
-                        } else {
-                            //Empty
-
-                        }
-                    }
-                    is Result.Error -> {
-                        //Error
-
+            when (val result = getFolderUseCase.execute()) {
+                is Result.Success -> {
+                    if (result.data.isNotEmpty()) {
+                        _folderItems.value = result.data.map(DomainEntity.Folder::mapToPresentation)
+                        _folderState.value = true
+                    } else {
+                        _folderState.value = false
                     }
                 }
+                is Result.Error -> Timber.d(result.exception)
             }
         }
+    }
+
+    val onItemClickListener = object: ItemClickListener {
+        override fun <T> onItemClicked(item: T) {
+            when (item) {
+                is PresentationEntity.Folder -> folderClick.value = item.id
+                is PresentationEntity.Category -> categoryClick.value = item.id
+            }
+        }
+    }
+
+    fun onAddClicked() {
+        addClick.call()
     }
 }
--- presentation/src/main/java/com/ddd4/dropit/presentation/util/Constants.kt
+++ presentation/src/main/java/com/ddd4/dropit/presentation/util/Constants.kt
@@ -6,4 +6,8 @@ object Constants {
     const val PERMISSION_CODE_CAMERA = 1001
     const val PERMISSION_CODE_READ_STORAGE = 1002
     const val PERMISSION_CODE_WRITE_STORAGE = 1003
+
+    //Extra Value ID
+    const val EXTRA_NAME_FOLDER_ID = "folderId"
+    const val EXTRA_NAME_CATEGORY_ID = "categoryId"
 }
