ed7b4979b326cda1560c57478ac0f3e3bb27d19d
jsh <ppm_it@naver.com>
2020-09-30 03:04:11 +0900

    fix: result success, error 판단 함수 추가

--- domain/src/main/java/com/ddd4/dropit/domain/Result.kt
+++ domain/src/main/java/com/ddd4/dropit/domain/Result.kt
@@ -4,3 +4,13 @@ sealed class Result<out R> {
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 }
+
+fun <T>Result<T>.getValue(): T {
+
+    return if(this is Result.Success) {
+        this.data
+    } else {
+        throw (this as Result.Error).exception
+    }
+
+}
